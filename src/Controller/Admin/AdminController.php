0a309fdf64fccc0dadf3f99262e9c0f862a66d73
mlraleader <mouwenlegan@gmail.com>
2020-10-02 16:22:03 +0100

    Ajout du bundle EasyAdmin et Définition des entité à gérer:User & AppelProjet

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class AdminController extends AbstractDashboardController
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Azobe');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'icon class', EntityClass::class);
     }
 }
